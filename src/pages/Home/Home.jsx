7d8de74cafeea970723c5ee2e4b2c397a77797ab
Wendel <wendel.duarte@proton.me>
2025-10-21 06:20:06 -0300

    initiated Home's content

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,25 @@
 import "./Home.css";
 import Header from "../../components/Header/Header";
+import iconReload from "../../assets/icon-reload.svg";
 
 const Home = () => {
-  return <Header />;
+  return (
+    <div className="Home">
+      <Header />
+      <main>
+        <div className="limiter">
+          <section className="container">
+            <div className="title-wrp">
+              <h2>Gastos</h2>
+              <button type="button">
+                <img src={iconReload} alt="Recarregar" />
+              </button>
+            </div>
+          </section>
+        </div>
+      </main>
+    </div>
+  );
 };
 
 export default Home;
